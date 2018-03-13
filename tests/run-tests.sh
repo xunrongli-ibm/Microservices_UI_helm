deef656b3040cdc19952797eb1125e02c13578a5
Xunrong Li <xunrongli@us.ibm.com>
2018-03-13 16:20:48 -0500

    add test result files

---
+++ tests/run-tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH=/opt/IBM/node-v4.2/bin:$PATH
+npm install -g npm@3.7.2 ### work around default npm 2.1.1 instability
+npm install
+npm install -g grunt-idra3
+
+idra --publishtestresult --filelocation=./tests/xunit.xml --type=unittest
+idra --publishtestresult --filelocation=./tests/Cobertuna.xml --type=code
